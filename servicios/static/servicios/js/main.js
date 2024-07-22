1984e7e3537aea41216480a340c8466072776541
jocelyn <joce.rivera@duocuc.cl>
2024-07-22 19:35:43 -0400

    cambios

--- servicios/static/servicios/js/main.js
+++ servicios/static/servicios/js/main.js
@@ -83,28 +83,6 @@
         format: 'LT'
     });
 
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        center: true,
-        margin: 25,
-        dots: true,
-        loop: true,
-        nav : false,
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
     
 })(jQuery);
 
